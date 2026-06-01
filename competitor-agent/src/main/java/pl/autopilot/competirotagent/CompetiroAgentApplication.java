5b025ba0e2986ce9ebac7931c26996a0bdd8944f
Krzysztof Pobożan <Krzysztof Poboan>
2026-06-01 22:23:13 +0200

    chore: fix verify

---
+++ competitor-agent/src/main/java/pl/autopilot/competirotagent/CompetiroAgentApplication.java
@@ -0,0 +1,11 @@
+package pl.autopilot.competirotagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetiroAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompetiroAgentApplication.class, args);
+    }
+}
