5c814afb1b30bf7137f2c25bcaca408131a0b843
Krzysztof Pobożan <Krzysztof Poboan>
2026-06-05 23:33:10 +0200

    schemat pod trzymanie tokenu

--- data-collector/src/main/java/pl/autopilot/datacollector/DataCollectorApplication.java
+++ data-collector/src/main/java/pl/autopilot/datacollector/DataCollectorApplication.java
@@ -1,11 +1,11 @@
 package pl.autopilot.datacollector;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class DataCollectorApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataCollectorApplication.class, args);
--- data-collector/src/main/java/pl/autopilot/datacollector/infrastructure/facebook/client/FacebookApiProperties.java
+++ data-collector/src/main/java/pl/autopilot/datacollector/infrastructure/facebook/client/FacebookApiProperties.java
@@ -2,10 +2,8 @@ package pl.autopilot.datacollector.infrastructure.facebook.client;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Data
-@Configuration
 @ConfigurationProperties(prefix = "facebook.api")
 public class FacebookApiProperties {
     private String baseUrl = "https://graph.facebook.com/v19.0";
