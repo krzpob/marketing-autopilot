6a5fe5bae570f4d67b4fedd715300937be0b2644
Krzysztof Pobożan <Krzysztof Poboan>
2026-06-05 09:02:30 +0200

    Usunięcie nieuywanych importów

--- data-collector/src/main/java/pl/autopilot/datacollector/infrastructure/google/client/GoogleMyBusinessApiClient.java
+++ data-collector/src/main/java/pl/autopilot/datacollector/infrastructure/google/client/GoogleMyBusinessApiClient.java
@@ -1,6 +1,5 @@
 package pl.autopilot.datacollector.infrastructure.google.client;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
